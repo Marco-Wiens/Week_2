015d91200f7963f7d7df43adad0894f78a518291
Marco-Wiens <marco.wiens@gmail.com>
2023-10-19 17:52:51 +0200

    Cambio nombre variable de tri a myTriangle

--- algebraProject/triangleTest.ts
+++ algebraProject/triangleTest.ts
@@ -5,5 +5,5 @@ import { Point } from "./point";
 let punto: Point = new Point(3,4);
 let punto2: Point = new Point(1,5);
 let punto3: Point = new Point(7,1);
-let tri: Triangle = new Triangle(punto,punto2,punto3);
-console.log(tri.calculateLengthSides());
+let myTriangle: Triangle = new Triangle(punto,punto2,punto3);
+console.log(myTriangle.calculateLengthSides());
