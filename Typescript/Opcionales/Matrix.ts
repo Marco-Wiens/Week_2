982f1f1e7f180faa32fb9fef4d439350dc0b6b55
Marco-Wiens <marco.wiens@gmail.com>
2023-10-19 10:32:04 +0200

    Opcionales Matrix Retos 1 y 2

--- Typescript/Opcionales/Matrix.ts
+++ Typescript/Opcionales/Matrix.ts
@@ -16,41 +16,22 @@ export class Matrix{
         console.log(this.elements);
     }
 
-    private verLength(matriz1,matriz2){
-        let vallen1: boolean = false;
-        if (matriz1.length == matriz2.length){
-            for(let m in matriz1){
-                for(let i=0; i< matriz1.length; i++){
-                    if(matriz1[i].length == matriz2[i].length){
-                        vallen1 = true;
-                    }else{
-                        // En caso de que sea falso sale del bucle
-                        vallen1 = false;
-                        return vallen1;
-                    }
-                }
-            }
-        }
-        return vallen1;
-    }
-
     public add(m1: Matrix):Matrix{
         let mat1: Matrix = new Matrix(0,0,0);
+        for(let i=0; i<this.elements.length; i++){
             let arr2: Vector = new Vector(0,0);
-                for(let i=0; i<this.elements.length; i++){
-                    for(let j=0; j<this.elements[i].elements.length;j++){
-                        arr2.elements.push(m1.elements[i].elements[j] + this.elements[i].elements[j]);
-                    }
-                    mat1.elements.push(arr2);
-                }
-        
+            for(let j=0; j<this.elements[i].elements.length;j++){
+                arr2.elements.push(m1.elements[i].elements[j] + this.elements[i].elements[j]);
+            }
+            mat1.elements.push(arr2);
+        }
         return mat1;
     }
 
     public multNumber(n:number):Matrix{
         let mat1: Matrix = new Matrix(0,0,0);
-        let arr2: Vector = new Vector(0,0);
         for(let i=0; i<this.elements.length; i++){
+            let arr2: Vector = new Vector(0,0);
             for(let j=0; j<this.elements[i].elements.length;j++){
                 arr2.elements.push(n * this.elements[i].elements[j]);
             }
@@ -61,14 +42,14 @@ export class Matrix{
 
     public mutSpecial(n:number):Matrix{
         let mat1: Matrix = new Matrix(0,0,0);
-        let arr2: Vector = new Vector(0,0);
+        
         for(let i=0; i<this.elements.length; i++){
+            let arr2: Vector = new Vector(0,0);
             for(let j=0; j<this.elements[i].elements.length;j++){
                 let num: number = this.elements[i].elements[j];
                 if(num%2 ==0){
                     arr2.elements.push(n * this.elements[i].elements[j]);
                 }else{
-
                     arr2.elements.push((n-1) * this.elements[i].elements[j]);
                 }
             }
--- Typescript/Opcionales/matrixTest.ts
+++ Typescript/Opcionales/matrixTest.ts
@@ -5,6 +5,7 @@ let mat2: Matrix = new Matrix(2,4,10);
 
 
 mat1.print();
+mat2.print();
 let mat3: Matrix = mat1.add(mat2);
 mat3.print();
 let mat4: Matrix = mat1.multNumber(3);
