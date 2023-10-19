6775061f4348f2dad062b9054c5c53e747bbe482
Marco-Wiens <marco.wiens@gmail.com>
2023-10-19 17:02:40 +0200

    Fundamentos 4: Reto 2: Se añaden métodos que calculan distancias entre dos puntos cualquiera y la distancia entre el punto de origen(0,0) y un punto creado.

--- algebraProject/point.ts
+++ algebraProject/point.ts
@@ -29,4 +29,12 @@ export class Point{
     public toString(): void{
         console.log("(" + this.x + "," + this.y + ")");
     }
+
+    public distanceToOrigin():number{
+        return Math.sqrt(Math.pow(this.x , 2) + Math.pow(this.y , 2));
+    }
+
+    public calculateDistance(anotherPoint: Point):number{
+        return Math.sqrt(Math.pow((anotherPoint.getX()-this.x),2) + Math.pow((anotherPoint.getY()-this.y),2));
+    }
 }
--- algebraProject/pointTest.ts
+++ algebraProject/pointTest.ts
@@ -1,9 +1,12 @@
 import { Point } from "./point";
 
 let punto: Point = new Point(3,4);
+let punto2: Point = new Point(3,4);
 
 punto.setX(4);
 punto.setY(5);
 console.log(punto.getX());
 console.log(punto.getY());
 punto.toString();
+console.log(punto.distanceToOrigin());
+console.log(punto.calculateDistance(punto2));
